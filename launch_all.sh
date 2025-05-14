ed79f5a64162455cea3fbfa4339664cb259be5f2
shujun <shujun@slogin001.cm.cluster>
2025-05-14 16:43:58 -0700

    simple version to run with onemill

--- launch_all.sh
+++ launch_all.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
 sbatch slurm_scripts/job_0.sh
-sbatch slurm_scripts/job_1.sh
-sbatch slurm_scripts/job_2.sh
-sbatch slurm_scripts/job_3.sh
-sbatch slurm_scripts/job_4.sh
-sbatch slurm_scripts/job_5.sh
-sbatch slurm_scripts/job_6.sh
-sbatch slurm_scripts/job_7.sh
-sbatch slurm_scripts/job_8.sh
-sbatch slurm_scripts/job_9.sh
-sbatch slurm_scripts/job_10.sh
-sbatch slurm_scripts/job_11.sh
-sbatch slurm_scripts/job_12.sh
-sbatch slurm_scripts/job_13.sh
-sbatch slurm_scripts/job_14.sh
-sbatch slurm_scripts/job_15.sh
