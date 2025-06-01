6ba6117b46c0d2656707bb4fe26fff0a93f9baae
Michael Innerberger <innerbergerm@hhmi.org>
2025-06-01 16:04:20 -0400

    Add a few vmap-based versions of the triangle update

--- jax_tmm_variations.py
+++ jax_tmm_variations.py
@@ -5,6 +5,7 @@ import jax
 import jax.numpy as jnp
 import flax.nnx as nnx
 from flax.nnx.rnglib import Rngs
+from tqdm import trange
 
 
 def benchmark(
@@ -18,7 +19,7 @@ def benchmark(
 ):
     times = []
     func = jax.jit(func)
-    for _ in range(n):
+    for _ in trange(n):
         start = time()
         result = func(x, mask).block_until_ready()
         end = time()
@@ -161,7 +162,7 @@ class TriangleMultiplicativeModuleMatmul(nnx.Module):
 
 
 class TriangleMultiplicativeModuleOptimizedTranspose(nnx.Module):
-    """Use transpose + matmul but try do transpositions before expensive matmul."""
+    """Use transpose + matmul but try to do transpositions before expensive matmul."""
     def __init__(
         self,
         *,
@@ -222,6 +223,177 @@ class TriangleMultiplicativeModuleOptimizedTranspose(nnx.Module):
         return self.to_out(out)
 
 
+class TriangleMultiplicativeModuleDot(nnx.Module):
+    """vmap-based matmul to see how this relates to built-in matmul."""
+    def __init__(
+        self,
+        *,
+        dim: int,
+        hidden_dim: int | None = None,
+        mix: Literal["ingoing", "outgoing"] = "ingoing",
+    ):
+        assert mix in {"ingoing", "outgoing"}, "mix must be either ingoing or outgoing"
+        rngs = Rngs(0)  # Use a fixed RNG for reproducibility
+
+        hidden_dim = hidden_dim if hidden_dim is not None else dim
+        self.norm = nnx.LayerNorm(dim, rngs=rngs)
+
+        self.left_proj = nnx.Linear(dim, hidden_dim, rngs=rngs)
+        self.right_proj = nnx.Linear(dim, hidden_dim, rngs=rngs)
+
+        self.left_gate = nnx.Linear(dim, hidden_dim, rngs=rngs)
+        self.right_gate = nnx.Linear(dim, hidden_dim, rngs=rngs)
+        self.out_gate = nnx.Linear(dim, hidden_dim, rngs=rngs)
+
+        if mix == "outgoing":
+            self.perm_left, self.perm_right = (0, 3, 1, 2), (0, 3, 2, 1)
+        elif mix == "ingoing":
+            self.perm_left, self.perm_right = (0, 3, 2, 1), (0, 3, 1, 2)
+        self.batched_matmul = jax.vmap(
+            jax.vmap(
+                self._my_matmul,
+                in_axes=(0, 0),
+                out_axes=0,
+            ),
+            in_axes=(0, 0),
+            out_axes=0,
+        )
+
+        self.to_out_norm = nnx.LayerNorm(hidden_dim, rngs=rngs)
+        self.to_out = nnx.Linear(hidden_dim, dim, rngs=rngs)
+
+    def _my_matmul(
+        self,
+        a: jnp.ndarray,
+        b: jnp.ndarray,
+    ) -> jnp.ndarray:
+        mv = jax.vmap(jnp.vdot, (0, None), 0)
+        mm = jax.vmap(mv, (None, 1), 1)
+        return mm(a, b)
+
+    def __call__(self, x: jnp.ndarray, src_mask: jnp.ndarray) -> jnp.ndarray:
+        src_mask = src_mask.astype(jnp.float32)
+        src_mask = jnp.expand_dims(src_mask, axis=-1)
+        # (B, L, 1) * (B, 1, L) -> (B, L, L)
+        mask = jnp.matmul(src_mask, src_mask.transpose((0, 2, 1)))
+        mask = jnp.expand_dims(mask, axis=-1)
+
+        assert x.shape[1] == x.shape[2], "feature map must be symmetrical"
+
+        x = self.norm(x)
+
+        left = self.left_proj(x)
+        right = self.right_proj(x)
+
+        left = left * mask
+        right = right * mask
+
+        left_gate = jax.nn.sigmoid(self.left_gate(x))
+        right_gate = jax.nn.sigmoid(self.right_gate(x))
+
+        out_gate = jax.nn.sigmoid(self.out_gate(x))
+
+        left = left * left_gate
+        right = right * right_gate
+
+        out = self.batched_matmul(
+            left.transpose(self.perm_left),
+            right.transpose(self.perm_right)
+        ).transpose((0, 2, 3, 1))
+
+        out = self.to_out_norm(out)
+        out = out * out_gate
+        return self.to_out(out)
+
+
+class TriangleMultiplicativeModuleDotOptimized(nnx.Module):
+    """vmap-based matmul without transpositions."""
+    def __init__(
+        self,
+        *,
+        dim: int,
+        hidden_dim: int | None = None,
+        mix: Literal["ingoing", "outgoing"] = "ingoing",
+    ):
+        assert mix in {"ingoing", "outgoing"}, "mix must be either ingoing or outgoing"
+        rngs = Rngs(0)  # Use a fixed RNG for reproducibility
+
+        hidden_dim = hidden_dim if hidden_dim is not None else dim
+        self.norm = nnx.LayerNorm(dim, rngs=rngs)
+
+        self.left_proj = nnx.Linear(dim, hidden_dim, rngs=rngs)
+        self.right_proj = nnx.Linear(dim, hidden_dim, rngs=rngs)
+
+        self.left_gate = nnx.Linear(dim, hidden_dim, rngs=rngs)
+        self.right_gate = nnx.Linear(dim, hidden_dim, rngs=rngs)
+        self.out_gate = nnx.Linear(dim, hidden_dim, rngs=rngs)
+
+        # Note: both of these are equivalent to a simple matmul, but will serve as
+        # a blueprint for a local update later
+        if mix == "outgoing":
+            def _my_matmul(
+                a: jnp.ndarray,
+                b: jnp.ndarray,
+            ) -> jnp.ndarray:
+                # This is a little bit slower then "ingoing" because of different memory access patterns
+                mv = jax.vmap(jnp.vdot, (0, None), 0)
+                mm = jax.vmap(mv, (None, 0), 1)
+                return mm(a, b)
+        elif mix == "ingoing":
+            def _my_matmul(
+                a: jnp.ndarray,
+                b: jnp.ndarray,
+            ) -> jnp.ndarray:
+                # This is fast because it accesses memory of a and b contiguously
+                mv = jax.vmap(jnp.vdot, (1, None), 0)
+                mm = jax.vmap(mv, (None, 1), 1)
+                return mm(a, b)
+        self.batched_matmul = jax.vmap(
+            jax.vmap(
+                _my_matmul,
+                in_axes=(2, 2),
+                out_axes=2,
+            ),
+            in_axes=(0, 0),
+            out_axes=0,
+        )
+
+        self.to_out_norm = nnx.LayerNorm(hidden_dim, rngs=rngs)
+        self.to_out = nnx.Linear(hidden_dim, dim, rngs=rngs)
+
+
+    def __call__(self, x: jnp.ndarray, src_mask: jnp.ndarray) -> jnp.ndarray:
+        src_mask = src_mask.astype(jnp.float32)
+        src_mask = jnp.expand_dims(src_mask, axis=-1)
+        # (B, L, 1) * (B, 1, L) -> (B, L, L)
+        mask = jnp.matmul(src_mask, src_mask.transpose((0, 2, 1)))
+        mask = jnp.expand_dims(mask, axis=-1)
+
+        assert x.shape[1] == x.shape[2], "feature map must be symmetrical"
+
+        x = self.norm(x)
+
+        left = self.left_proj(x)
+        right = self.right_proj(x)
+
+        left = left * mask
+        right = right * mask
+
+        left_gate = jax.nn.sigmoid(self.left_gate(x))
+        right_gate = jax.nn.sigmoid(self.right_gate(x))
+
+        out_gate = jax.nn.sigmoid(self.out_gate(x))
+
+        left = left * left_gate
+        right = right * right_gate
+
+        out = self.batched_matmul(left, right)
+
+        out = self.to_out_norm(out)
+        out = out * out_gate
+        return self.to_out(out)
+
+
 
 if __name__ == "__main__":
     SHAPE = (8, 177, 177, 128)  # (Batch size, Sequence length, Feature dimension)
@@ -246,3 +418,11 @@ if __name__ == "__main__":
     print("\nOptimized Transpose + Matmul")
     optimized = TriangleMultiplicativeModuleOptimizedTranspose(dim=SHAPE[3], mix=IN_OR_OUT)
     benchmark(optimized, x, mask, expected, n=N)
+
+    print("\nDot product-based matmul")
+    dot = TriangleMultiplicativeModuleDot(dim=SHAPE[3], mix=IN_OR_OUT)
+    benchmark(dot, x, mask, expected, n=N)
+
+    print("\nDot product-based matmul (optimized)")
+    dot_optimized = TriangleMultiplicativeModuleDotOptimized(dim=SHAPE[3], mix=IN_OR_OUT)
+    benchmark(dot_optimized, x, mask, expected, n=N)
